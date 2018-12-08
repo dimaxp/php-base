8d3fe3a418f66a8774ec9e9db40e51d1fb10d5f7
dasha <frgyvu@gmail.com>
2018-12-08 10:12:30 +0200

    lesson 7: ajax запрос

---
+++ session/ajax/auth_form_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+    <form action="account.php" method="post">
+        <div>
+            <input type="text" name="title" placeholder="title">
+        </div>
+        <div>
+            <input type="file" name="file">
+        </div>
+        <div>
+            <input type="submit" value="Send">
+        </div>
+    </form>
+
+<script src="js/sendForm.js"></script>
+</body>
+</html>
